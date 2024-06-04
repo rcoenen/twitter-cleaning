21d09a9531f6bd0f953dd6af0278a03a0ee38eb7
Rob Coenen <coenen.rob@gmail.com>
2024-06-03 23:13:15 -0400

    Fix retweet deletion script and update workflow

--- deleteRetweets.js
+++ deleteRetweets.js
@@ -3,20 +3,23 @@ const { ApiResponseError } = require('twitter-api-v2');
 
 const deleteRetweets = async () => {
   const user = await twitter.v2.me();
-  const retweets = await twitter.v2.userTimeline(user.data.id, { 'tweet.fields': 'created_at', 'exclude': 'tweets' });
+  const retweets = await twitter.v2.userTimeline(user.data.id, { 
+    'tweet.fields': 'created_at', 
+    'exclude': 'retweets,replies' 
+  });
 
-  for await (const retweet of retweets) {
+  for await (const tweet of retweets) {
     try {
-      if (Date.parse(retweet.created_at) <= treshold) {
-        console.log(`Deleting retweet ${retweet.id}`);
+      if (tweet.retweeted_status && Date.parse(tweet.created_at) <= treshold) {
+        console.log(`Deleting retweet ${tweet.id}`);
 
-        await twitter.v1.deleteTweet(retweet.id);
+        await twitter.v1.deleteTweet(tweet.id_str);
       }
     } catch (error) {
       if (error instanceof ApiResponseError && error.rateLimitError && error.rateLimit) {
         console.log('Rate limit hit, waiting for the timer reset (this can take up to 15 minutes)');
         await sleep(error.rateLimit.reset);
-        continue
+        continue;
       }
 
       throw error;
